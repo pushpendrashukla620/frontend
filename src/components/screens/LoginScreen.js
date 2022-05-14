81b3f74da5d9548210ead5ed485ff2e89e646ad7
Pushpendra Shukla <pushpendrashukla620@gmail.com>
2022-05-14 19:08:52 +0530

    commit

---
+++ src/components/screens/LoginScreen.js
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "./LoginScreen.css";
+
+const LoginScreen = ({ history }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+
+
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    
+
+    try {
+      console.log(localStorage.authToken)
+      if (localStorage.authToken){
+        history.push("/");
+      }
+   
+      const { data } = await axios.post(
+        "/api/auth/login",
+        { email, password },
+      );
+
+      localStorage.setItem("authToken", data.data.AccessToken);
+      console.log(localStorage)
+      history.push("/")
+
+
+    } catch (error) {
+      console.log(error)
+
+    }
+  };
+
+  return (
+    <div className="login-screen">
+      <form onSubmit={loginHandler} className="login-screen__form">
+        <h3 className="login-screen__title">Login</h3>
+        {error && <span className="error-message">{error}</span>}
+        <div className="form-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            required
+            id="email"
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            tabIndex={1}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">
+            Password:{" "}
+            <Link to="/forgotpassword" className="login-screen__forgotpassword">
+              Forgot Password?
+            </Link>
+          </label>
+          <input
+            type="password"
+            required
+            id="password"
+            autoComplete="true"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            tabIndex={2}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+
+        <span className="login-screen__subtext">
+          Don't have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default LoginScreen;
---
+++ src/components/screens/PrivateScreen.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./PrivateScreen.css";
+
+const PrivateScreen = () => {
+  const [error, setError] = useState("");
+  const [privateData, setPrivateData] = useState("");
+
+  useEffect(() => {
+    const fetchPrivateDate = async () => {
+
+      try {
+        const { data } = await axios.get("/api/auth/private");
+        setPrivateData(data.data);
+      } catch (error) {
+        localStorage.removeItem("authToken");
+        setError("You are not authorized please login");
+      }
+    };
+
+    fetchPrivateDate();
+  }, []);
+  return error ? (
+    <span className="error-message">{error}</span>
+  ) : (
+    <div>{privateData}</div>
+  );
+};
+
+export default PrivateScreen;
